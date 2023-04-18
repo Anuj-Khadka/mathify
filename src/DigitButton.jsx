dab46185fa514ed0f6d663f911ac940e86fef47c
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-18 23:24:26 +0545

    add numeric buttons

---
+++ src/DigitButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { actions } from "./App";
+
+const DigitButton = ({ dispatch, digit }) => {
+  return (
+    <button
+      onClick={() => dispatch({ type: actions.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+};
+
+export default DigitButton;
