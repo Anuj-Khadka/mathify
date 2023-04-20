3d58a3f1f380be037fc14a0dc67295dd3a5d699d
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-20 21:41:29 +0545

    update mathify

--- src/OperationButton.jsx
+++ src/OperationButton.jsx
@@ -4,7 +4,7 @@ import { actions } from "./App";
 const OperationButton = ({ dispatch, operation }) => {
   return (
     <button
-      onClick={() => dispatch({ type: actions.OPERATION, payload: { operation } })}
+      onClick={() => dispatch({ type: actions.CHOOSE_OPERATION, payload: { operation } })}
     >
       {operation}
     </button>
